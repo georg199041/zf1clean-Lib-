b342c7daefc2b7e69a85efc1e9cfff81d9f993c4
Павленко Евгений <pavlenko.obs@gmail.com>
2012-12-04 17:02:46 +0200

    Master/Slave cache system

--- Core/Cache/Backend/SlaveFile.php
+++ Core/Cache/Backend/SlaveFile.php
@@ -38,74 +38,184 @@ require_once 'Zend/Cache/Backend/Interface.php';
  */
 class Core_Cache_Backend_SlaveFile extends Zend_Cache_Backend implements Zend_Cache_Backend_Interface
 {
-	protected function _getMetadatas($id = null)
-	{
-		
-	}
+    /**
+     * Backend specific options
+     * 
+     * @var array
+     */
+	protected $_options = array(
+        'cache_dir' => null,
+    );
 	
-	protected function _loadMetadatas($id = null)
+	/**
+	 * Chech if record expired
+	 * 
+	 * @param  integer $mtime Record modified time
+	 * @return boolean
+	 */
+	public function isExpiriedMTime($mtime)
 	{
-		
+		return (time() > $mtime + $this->getLifetime(false));
 	}
+    
+    /**
+     * Get and check cache dir
+     * 
+     * @throws Zend_Cache_Exception If 'cache_dir' is not a directory or not writable
+     * @return string
+     */
+    public function getCacheDir()
+    {
+    	if (!is_dir($this->_options['cache_dir'])) {
+            Zend_Cache::throwException('cache_dir must be a directory');
+        }
+        
+        if (!is_writable($this->_options['cache_dir'])) {
+            Zend_Cache::throwException('cache_dir is not writable');
+        }
+        
+        return $this->_options['cache_dir'];
+    }
+    
+    /**
+     * Get file path by ID
+     * 
+     * @param  string $id Cache id hash
+     * @return string     Path to file
+     */
+    public function getCachedFilePath($id)
+    {
+    	return $this->getCacheDir() . DIRECTORY_SEPARATOR . $id;
+    }
+    
+    /**
+     * Get metadata mtime
+     * 
+     * @param  string $id Cache id hash
+     * @return integer mtime timestamp
+     */
+    public function getCachedFileMTime($id)
+    {
+    	return (int) @file_get_contents($this->getCachedFilePath($id) . '.mtime');
+    }
 	
+    /**
+     * Load cached file path
+     * 
+     * @param  string  $id                     Cache id hash
+     * @param  boolean $doNotTestCacheValidity [OPTIONAL] Unused in this backend
+     * @return boolean|string                  Path to file if exists or false otherwise
+     */
 	public function load($id, $doNotTestCacheValidity = false)
 	{
+		$this->_log(__CLASS__ . ": load item '{$id}'", Zend_Log::DEBUG);
 		if (!$this->test($id)) {
 			return false;
 		}
 		 
-		return self::$_container[$id];
+		return $this->getCachedFilePath($id);
 	}
 
+	/**
+	 * Test cache exists
+	 * 
+	 * @param  string $id Cache id hash
+	 * @return boolean|integer Last modified of cached file or false if not exists cache
+	 */
     public function test($id)
 	{
-		return is_file($id) && file_exists($id);
-		return isset(self::$_container[$id]) && !is_null(self::$_container[$id]);
+		$this->_log(__CLASS__ . ": test item '{$id}'", Zend_Log::DEBUG);
+		$file  = $this->getCachedFilePath($id);
+		if (file_exists($file) && is_file($file)) {
+			return $this->getCachedFileMTime($id);
+		}
+		
+		return false;
 	}
-
-    public function save($data, $id, $tags = array(), $specificLifetime = false)
+	
+	/**
+	 * Save file metadata
+     * 
+     * @param  mixed  $data             Mtime value
+     * @param  string $id               Cache id hash
+     * @param  array  $tags             [OPTIONAL] Unused in this backend
+     * @param  int    $specificLifetime [OPTIONAL] Unused in this backend
+     * @param  int    $priority         [OPTIONAL] Unused in this backend
+	 * @return boolean Returns true if success
+	 */
+	public function save($data, $id, $tags = array(), $specificLifetime = false)
 	{
-		self::$_container[$id] = $data;
-		if (count($tags) > 0) {
-			$this->_log("Core_Cache_Backend_Runtime::save() : tags are unsupported by the runtime backend");
+		$this->_log(__CLASS__ . ": save item '{$id}'", Zend_Log::DEBUG);
+		$mtime = file_put_contents($this->getCachedFilePath($id) . '.mtime', (string) $data);
+		if (!$mtime) {
+			$this->_log(__CLASS__ . ": can't save item mtime '{$id}'", Zend_Log::DEBUG);
+			return false;
 		}
 		
 		return true;
 	}
 
-    public function remove($id)
+	/**
+	 * Remove specified file cache
+	 * 
+	 * @param  string  $id Cache id hash
+	 * @return boolean Success flag
+	 */
+	public function remove($id)
 	{
-		unset(self::$_container[$id]);
-		return true;
+		$this->_log(__CLASS__ . ": remove item '{$id}'", Zend_Log::DEBUG);
+		$file = $this->getCachedFilePath($id);
+		return @unlink($file);
 	}
 
+	/**
+	 * Clean all or old entries from cache
+	 * 
+	 * @param  string $mode Cleaning mode
+	 * @param  array  $tags [OPTIONAL] Unused in this backend
+	 * @return boolean Success flag
+	 */
     public function clean($mode = Zend_Cache::CLEANING_MODE_ALL, $tags = array())
 	{
-		if ($mode == Zend_Cache::CLEANING_MODE_ALL) {
-			self::$_container = array();
-			$this->_options['cache_dir'];
-		}
-		
-		if ($mode == Zend_Cache::CLEANING_MODE_OLD) {
-			$this->_log("Core_Cache_Backend_Runtime::clean() : CLEANING_MODE_OLD is unsupported by the session backend");
+		$this->_log(__CLASS__ . ": clean backend", Zend_Log::DEBUG);
+		if ($mode == Zend_Cache::CLEANING_MODE_ALL || $mode == Zend_Cache::CLEANING_MODE_OLD) {
+			$dir = new DirectoryIterator($this->getCacheDir());
+			foreach ($dir as $file) {
+				if (!$file->isFile()) {
+					// remove any directories ???
+					continue;
+				}
+				
+				if ($mode == Zend_Cache::CLEANING_MODE_ALL) {
+					// remove CLEANING_MODE_ALL
+					unlink($file->getPathname());
+					continue;
+				}
+				
+				if ($this->isExpiriedMTime($file->getMTime())) {
+					// remove CLEANING_MODE_OLD
+					unlink($file->getPathname());
+				}
+			}
 		}
 		
 		if ($mode == Zend_Cache::CLEANING_MODE_MATCHING_TAG) {
-			$this->_log("Core_Cache_Backend_Runtime::clean() : tags are unsupported by the session backend");
+			$this->_log(__CLASS__ . ": tags are unsupported by this backend", Zend_Log::WARN);
 		}
 		
 		if ($mode == Zend_Cache::CLEANING_MODE_NOT_MATCHING_TAG) {
-			$this->_log("Core_Cache_Backend_Runtime::clean() : tags are unsupported by the session backend");
+			$this->_log(__CLASS__ . ": tags are unsupported by this backend", Zend_Log::WARN);
 		}
+		return true;
 	}
 
 	/**
 	 * Return true if the automatic cleaning is available for the backend
 	 *
-	 * @return boolean
+	 * @return boolean Flag of autocleaning enabled
 	 */
 	public function isAutomaticCleaningAvailable()
 	{
-		return false; // Make true
+		return false;
 	}
 }
--- Core/Cache/Frontend/MasterFile.php
+++ Core/Cache/Frontend/MasterFile.php
@@ -23,7 +23,7 @@
 /**
  * @see Zend_Cache_Core
  */
-require_once 'Zend/Cache/Frontend/File.php';
+require_once 'Zend/Cache/Core.php';
 
 /**
  * @package    Core_Cache
@@ -31,21 +31,131 @@ require_once 'Zend/Cache/Frontend/File.php';
  * @copyright  Copyright (c) 2005-2012 SunNY Creative Technologies. (http://www.sunny.net.ua)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class Core_Cache_Frontend_MasterFile extends Zend_Cache_Frontend_File //TODO
+class Core_Cache_Frontend_MasterFile extends Zend_Cache_Core
 {
     /**
-     * Constructor
+     * Get hash of specified file path id
      * 
-     * Overriding check 'master_files' specific option to
-     * not throws an Exception while configuring
-     *
-     * @param  array $options Associative array of options
-     * @return void
+     * @param  string $id Cache id (path)
+     * @return string     String converted with md5() function
      */
-	public function __construct(array $options = array())
+	public function getIdHash($id)
+    {
+    	$id   = ltrim($id, '/\\');
+    	$info = pathinfo($id);
+    	return md5($info['dirname'] . DIRECTORY_SEPARATOR . $info['filename']) . '.' . $info['extension'];
+    }
+	
+	/**
+	 * Load cache data (path to cached file) (if enabled)
+	 * 
+	 * @param  string  $id                     Cache id
+	 * @param  boolean $doNotTestCacheValidity [OPTIONAL] Unused in this frontend
+	 * @param  boolean $doNotUnserialize       [OPTIONAL] Unused in this frontend
+	 * @return boolean|string                  Path to cached file or false if file not exists
+	 */
+	public function load($id, $doNotTestCacheValidity = false, $doNotUnserialize = false)
 	{
-		while (list($name, $value) = each($options)) {
-			$this->setOption($name, $value);
+		if (!$this->getOption('caching')) {
+			return false;
 		}
+		
+		$this->_log(__CLASS__ . ": load item '{$id}'", Zend_Log::DEBUG);
+		$hash = $this->getIdHash($id);
+		if ($this->test($id)) {
+			return $this->getBackend()->load($hash);
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Test cache exists (if enabled)
+	 * 
+	 * @param  string $id Cache id (path to file without basedir)
+	 * @return boolean|integer Last modified or false
+	 */
+	public function test($id)
+	{
+		if (!$this->getOption('caching')) {
+			return false;
+		}
+		
+		$this->_log(__CLASS__ . ": test item '{$id}'", Zend_Log::DEBUG);
+		$hash = $this->getIdHash($id);
+		return $this->getBackend()->test($hash);
+	}
+	
+	/**
+	 * Save file info
+	 * 
+     * @param  mixed  $data             Data (uses as id argument if single)
+     * @param  string $id               [OPTIONAL] Cache id hash
+     * @param  array  $tags             [OPTIONAL] Unused in this backend
+     * @param  int    $specificLifetime [OPTIONAL] Unused in this backend
+     * @param  int    $priority         [OPTIONAL] Unused in this backend
+	 * @return boolean Returns true if success
+	 */
+	public function save($data, $id = null, $tags = array(), $specificLifetime = false, $priority = 8)
+	{
+		$cid = $data;
+		if (null !== $id) {
+			$cid = $id;
+		}
+		$cid = ltrim($cid, '/\\');
+		if (!file_exists(PUBLIC_PATH . DIRECTORY_SEPARATOR . $cid)) {
+			$this->_log(__CLASS__ . ": item not exists '{$cid}'", Zend_Log::ERR);
+			return false;
+		}
+		
+		$mtime = (null === $id) ? filemtime(PUBLIC_PATH . DIRECTORY_SEPARATOR . $cid) : $data;
+		if ($mtime == 0) {
+			$this->_log(__CLASS__ . ": item mtime invalid '{$cid}'", Zend_Log::ERR);
+			return false;
+		}
+		
+		return $this->getBackend()->save($mtime, $this->getIdHash($cid));
+	}
+	
+	/**
+	 * Remove specified file cache
+	 * 
+	 * @param  string  $id Cache id (path to file without basedir)
+	 * @return boolean Success flag
+	 */
+	public function remove($id)
+	{
+		if (!$this->getOption('caching')) {
+			return true;
+		}
+		
+		$this->_log(__CLASS__ . ": remove item '{$id}'", Zend_Log::DEBUG);
+		$hash = $this->getIdHash($id);
+		return $this->getBackend()->remove($hash);
+	}
+	
+	/**
+	 * Clean all or old entries from cache
+	 * 
+	 * @param  string $mode Cleaning mode
+	 * @param  array  $tags [OPTIONAL] Unused in this backend
+	 * @return boolean Success flag
+	 */
+	public function clean($mode = 'all', $tags = array())
+	{
+		if (!$this->getOption('caching')) {
+			return true;
+		}
+		
+		if (!in_array($mode, array(Zend_Cache::CLEANING_MODE_ALL,
+				Zend_Cache::CLEANING_MODE_OLD,
+				Zend_Cache::CLEANING_MODE_MATCHING_TAG,
+				Zend_Cache::CLEANING_MODE_NOT_MATCHING_TAG,
+				Zend_Cache::CLEANING_MODE_MATCHING_ANY_TAG))) {
+			Zend_Cache::throwException('Invalid cleaning mode');
+		}
+		
+		$this->_log(__CLASS__ . ": clean cache", Zend_Log::DEBUG);
+		return $this->getBackend()->clean($mode, $tags);
 	}
 }
